358a180ff8a6db293a0508f70136cf2c1e490426
M0NIQUEE <moniquenguyen425@gmail.com>
2025-04-30 22:43:48 -0700

    2

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,4 +1,4 @@
-const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
+const char *dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
 /*
  * This routine performs a dgemv operation
@@ -6,9 +6,9 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
-       for (int i = 0; i < n; ++i) {
+void my_dgemv(int n, double *A, double *x, double *y) {
+    // insert your code here: implementation of basic matrix multiply
+    for (int i = 0; i < n; ++i) {
         double sum = 0.0;
         for (int j = 0; j < n; ++j) {
             sum += A[i * n + j] * x[j];
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,4 +1,4 @@
-const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
  * This routine performs a dgemv operation
@@ -6,7 +6,19 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of vectorized vector-matrix multiply
+void my_dgemv(int n, double *A, double *x, double *y) {
+    for (int i = 0; i < n; i++) {
+        double sum = 0.0;
 
+        for (int j = 0; j <= n - 4; j += 4) {
+            sum += A[i * n + j] * x[j] + A[i * n + j + 1] * x[j + 1] +
+                   A[i * n + j + 2] * x[j + 2] + A[i * n + j + 3] * x[j + 3];
+        }
+
+        for (int j = 0; j < n; j++) {
+            sum += A[i * n + j] * x[j];
+        }
+
+        y[i] += sum;
+    }
 }
