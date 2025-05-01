4cbd3c380a66b96d625cbac09c905e417ff2dcac
M0NIQUEE <moniquenguyen425@gmail.com>
2025-04-30 22:50:58 -0700

    2

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -1,9 +1,9 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-const char* dgemv_desc = "OpenMP dgemv.";
+const char *dgemv_desc = "OpenMP dgemv.";
 
 /*
  * This routine performs a dgemv operation
@@ -12,19 +12,28 @@ const char* dgemv_desc = "OpenMP dgemv.";
  * On exit, A and X maintain their input values.
  */
 
-void my_dgemv(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double *A, double *x, double *y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-      printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
+#pragma omp parallel
+    {
+        int nthreads = omp_get_num_threads();
+        int thread_id = omp_get_thread_num();
+        printf("my_dgemv(): Hello world: thread %d of %d checking in. \n",
+               thread_id, nthreads);
+        printf("my_dgemv(): For actual timing runs, please comment out these "
+               "printf() and omp_get_*() statements. \n");
+    }
 
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you will want to comment out the above parallel code block that prints out
-   // nthreads and thread_id so as to not taint your timings
+    // insert your dgemv code here. you may need to create additional parallel regions,
+    // and you will want to comment out the above parallel code block that prints out
+    // nthreads and thread_id so as to not taint your timings
 
+#pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < n; j++) {
+            sum += A[i * n + j] * x[j];
+        }
+        y[i] += sum;
+    }
 }
-
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -6,6 +6,11 @@ const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
+
+/*
+https://www.jsums.edu/robotics/files/2016/12/FECS17_Proceedings-FEC3555.pdf 
+also some help from chatgpt
+*/
 void my_dgemv(int n, double *A, double *x, double *y) {
     for (int i = 0; i < n; ++i) {
         double sum = 0.0;
